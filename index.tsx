f7bc50ee83dc42791ff6fe5ea095f8116fd70042
kevalhsglobalexports-afk <keval.hsglobalexports@gmail.com>
2026-02-06 12:54:55 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+const container = document.getElementById("root");
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (!container) {
+  throw new Error("Root container missing in index.html");
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
